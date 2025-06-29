dcf11bf40b96df4ed7600a7ae3e102cbd49231d4
Jakub Richardson <jakubr060104@gmail.com>
2025-06-29 19:12:47 +0100

    Outline VCU-IO functionality

--- VCU-IO/VCU-IO.ino
+++ VCU-IO/VCU-IO.ino
@@ -1,114 +1,206 @@
 #include <Fsm.h>
-#include <SPI.h>
-
-
-// Pins
-const int CHIP_SELECT_PIN = D7;
-const int AIR_PLUS = D0;
-const int AIR_MINUS = D1;
-const int PRE_CHARGE = D2;
-const int DISCHARGE_DISABLE = D3;
-const int SDC_PG = D4;
-
-// Input Buttons:
-// const int SPARE_INPUT = D5; // Will be used for autonomous TS-Activation
-const int ACTIVATION_BUTTON = D6;  // TS-Activation button input pin
-int activationButtonState; // current button reading
-int lastActivationButtonState = LOW;  // previous button reading
-
-// Timers
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastActivationDebounceTime = 0;  // the last time the output pin was toggled
-const unsigned long DEBOUNCE_DELAY = 20;   // the debounce time; increase if the output flickers
-int count = 0;
-bool sdcPowerGood = false;
-const unsigned long CONTACTOR_DELAY = 200; // 200
+#include <FlexCAN_T4.h>
 
 // Print Debug
 const bool LOG_STATE = true;
-const bool LOG_VOLT = true;
-const unsigned long LOG_VOLT_TIME = 500;
-unsigned long lastVoltLog = 0; // Last time voltage was logged
 
-//Events
-#define BUTTON_EVENT  0
-#define SDC_EVENT  1
-#define PRE_CHARGE_COMPLETE  2
+// CAN Bus
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
+const int BAUD_RATE = 500000;
+
+// Pins
+const int CAN_BUS_STBY = 6;
+int ANG_INP[2] = {14, 15}; // Analog Inputs
+int DIG_INP[8] = {11, 12, 21, 20, 19, 18, 17, 16}; // 24/5V Digital Inputs
+int DIG_OUT_5V[8] = {0, 1, 2, 3, 4, 5, 7, 8}; // 5V Digital Outputs
+int DIG_OUT_24V[2] = {9, 10}; // 24V Digital Outputs
+
+// Input signals:
+const int R2D_BUTTON = DIG_INP[1];  // R2D button input pin
+const int TSMS = DIG_INP[4]; // TSMS - 24V input
+int buttonState; // current button reading
+int lastButtonState = LOW;  // previous button reading
+// Timers:
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+const unsigned long DEBOUNCE_DELAY = 20;   // the debounce time; increase if the output flickers
+
+// Inverter RFE Message
+bool RFE = false;
+const int RFE_ID = 0x01; // TODO
+
+// Inverter Enable Message
+unsigned long lastEnableSendTime = 0;  // the last time enable was sent to the inverter
+const unsigned long ENABLE_CYCLE_TIME = 100;   // time (ms) between sending enable messages
+const int INV_ENBL_ID = 0x02; // TODO
+
+// Events
+#define TS_ACTIVATION_EVENT  0
+#define TS_DEACTIVATION_EVENT  1
+#define R2D_BUTTON_EVENT  2
 
 // FSM States
-State stateIdle(&onEnterStateIdle, NULL, NULL);
-State stateSDC(&onEnterSDC, NULL, NULL);
-Fsm fsm(&stateIdle);
+State stateTractiveSystemOff(&onEnterTractiveSystemOff, NULL, NULL);
+State stateTractiveSystemActive(&onEnterTractiveSystemActive, NULL, NULL);
+State stateManualDriving(&onEnterManualDriving, NULL, NULL);
+Fsm fsm(&stateTractiveSystemOff);
 
-void onEnterStateIdle() {
-  Serial.println("IDLE");
-  digitalWrite(DISCHARGE_DISABLE, LOW);
-  digitalWrite(AIR_MINUS, LOW);
-  digitalWrite(PRE_CHARGE, LOW);
-  digitalWrite(AIR_PLUS, LOW);
-}
 
-void onEnterSDC() {
-  if (LOG_STATE) {
-    Serial.println("SDC Opened");
+void handleReceive(const CAN_message_t &msg) {
+  if (msg.id == RFE_ID) {
+    readRFE(msg);
   }
 }
 
-void setupFSM() {
-  fsm.add_transition(&stateTractiveSystemActive, &stateSDC, SDC_EVENT, NULL);
-  fsm.add_timed_transition(&stateSDC, &stateIdle, 200, NULL);
-  fsm.add_transition(&stateCheckVoltage, &stateAirPlusClosed, PRE_CHARGE_COMPLETE, NULL);
+/**
+ * @brief Read the value of RFE from the Inverter
+ */
+void readRFE(const CAN_message_t &msg) {
+  // TODO: Complete
 }
 
-void setup() {
-  Serial.begin(9600);
-
-  // Setup Contactor Outputs
-  pinMode(AIR_PLUS, OUTPUT);
-  pinMode(AIR_MINUS, OUTPUT);
-  pinMode(PRE_CHARGE, OUTPUT);
-  pinMode(DISCHARGE_DISABLE, OUTPUT);
-  
-  // Setup Inputs
-  pinMode(SDC_PG, INPUT);
-  pinMode(ACTIVATION_BUTTON, INPUT);
-
-  // Setup SPI
-  pinMode(CHIP_SELECT_PIN, OUTPUT);
-  digitalWrite(CHIP_SELECT_PIN, HIGH);
-  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE3));
-  SPI.begin();
-
-  // Setup State Machine
-  setupFSM();
-  onEnterStateIdle(); // Ensure outputs start LOW
-}
-
-void loop() {
-  // Handle Activation Button
-  int activationButtonReading = digitalRead(ACTIVATION_BUTTON);
-  if (activationButtonReading != lastActivationButtonState) {
-    lastActivationDebounceTime = millis(); // Reset debounce timer
+/**
+ * @brief Send inverter enable CAN message
+ * 
+ * @param bool enable - send enable/disable
+ */
+void enableInverter(bool enable) {
+  if ((millis() - lastEnableSendTime) > ENABLE_CYCLE_TIME) {
+    lastEnableSendTime = millis();
+    // TODO: Complete
   }
-  if ((millis() - lastActivationDebounceTime) > DEBOUNCE_DELAY) {
-    if (activationButtonReading != activationButtonState) {
-      activationButtonState = activationButtonReading;
-      if (activationButtonState == LOW) { // Button Released
-        if (sdcPowerGood) {
-          count++;
-          fsm.trigger(BUTTON_EVENT);
-        }
+}
+
+/**
+ * @brief Reads button to check if pressed
+ * 
+ * @return bool - button pressed
+ */
+bool buttonPressed() {
+  bool pressed = false;
+  // Handle R2D Button
+  int buttonReading = digitalRead(R2D_BUTTON);
+  if (buttonReading != lastButtonState) {
+    lastDebounceTime = millis(); // Reset debounce timer
+  }
+  if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
+    if (buttonReading != buttonState) {
+      buttonState = buttonReading;
+      if (buttonState == LOW) { // Button Released
+        pressed = true;
       }
     }
   }
-  lastActivationButtonState = activationButtonReading;
+  lastButtonState = buttonReading;
+  return pressed;
+}
 
-  // check that SDC is closed
-  sdcPowerGood = digitalRead(SDC_PG); // SDC activated when SDC_PG >19.5V
-  if (!sdcPowerGood)  {
-    fsm.trigger(SDC_EVENT);
+void onEnterTractiveSystemOff() {
+  if (LOG_STATE) {
+    Serial.println("TS-Off");
   }
-  
+}
+
+void duringTractiveSystemOff() {
+  // 1. Read RFE. If RFE goes high go to TS-Active
+  if (RFE) {
+    fsm.trigger(TS_ACTIVATION_EVENT);
+  }
+}
+
+void onEnterTractiveSystemActive() {
+  if (LOG_STATE) {
+    Serial.println("TS Active");
+  }
+}
+
+void duringTractiveSystemActive() {
+  // 1. Read RFE. If goes low go to TS-Off
+  if (!RFE) {
+    fsm.trigger(TS_DEACTIVATION_EVENT);
+  }
+  // 2. Read button. If pressed go to Manual Driving
+  bool pressed = buttonPressed();
+  if (pressed) {
+    fsm.trigger(R2D_BUTTON_EVENT);
+  }
+  // 3. Send Inverter Disable
+  enableInverter(false);
+}
+
+void onEnterManualDriving() {
+  if (LOG_STATE) {
+    Serial.println("Manual Driving");
+  }
+  // If one-shot enable then put it here
+}
+
+void duringManualDriving() {
+  // 1. Read RFE. If goes low go to TS-Off
+  if (!RFE) {
+    fsm.trigger(TS_DEACTIVATION_EVENT);
+  }
+  // 2. Read button. If pressed go to TS-Active
+  bool pressed = buttonPressed();
+  if (pressed) {
+    fsm.trigger(R2D_BUTTON_EVENT);
+  }
+  // 3. Send Inverter Enable signal
+  enableInverter(true);
+}
+
+void setupFSM() {
+  // State: Tractive System Off
+  fsm.add_transition(&stateTractiveSystemOff, &stateTractiveSystemActive, TS_ACTIVATION_EVENT, NULL);
+
+  // State: Tractive System Active
+  fsm.add_transition(&stateTractiveSystemActive, &stateManualDriving, R2D_BUTTON_EVENT, NULL);
+  fsm.add_transition(&stateTractiveSystemActive, &stateTractiveSystemOff, TS_DEACTIVATION_EVENT, NULL);
+
+  // State: Manual Driving
+  fsm.add_transition(&stateManualDriving, &stateTractiveSystemActive, R2D_BUTTON_EVENT, NULL);
+  fsm.add_transition(&stateManualDriving, &stateTractiveSystemOff, TS_DEACTIVATION_EVENT, NULL);
+}
+
+void setupInputs(int *pins, int size) {
+  for(int i = 0; i < size; i++) {
+    pinMode(pins[i], INPUT);
+  }
+}
+
+void setupOutputs(int *pins, int size) {
+  for(int i = 0; i < size; i++) {
+    pinMode(pins[i], OUTPUT);
+    digitalWrite(pins[i], LOW);
+  }
+}
+
+void setup() {
+  // Initialise Serial Interface
+  Serial.begin(9600);
+
+  // Initilise Pins
+  // setupInputs(ANG_INP, 2); // Analog Inputs
+  setupInputs(DIG_INP, 8); // Digital Inputs
+  setupOutputs(DIG_OUT_5V, 8); // 5V Digital Outputs
+  setupOutputs(DIG_OUT_24V, 2); // 24V Digital Outputs
+
+  // Initialise CAN
+  pinMode(CAN_BUS_STBY, OUTPUT); digitalWrite(CAN_BUS_STBY, LOW); /* optional tranceiver enable pin */
+  Can0.begin();
+  Can0.setBaudRate(BAUD_RATE);
+  Can0.setMaxMB(16);
+  Can0.enableFIFO();
+  Can0.enableFIFOInterrupt();
+  Can0.onReceive(handleReceive);
+  Can0.mailboxStatus();
+
+  // Setup State Machine
+  setupFSM();
+  onEnterTractiveSystemOff();
+}
+
+void loop() {
   fsm.run_machine();
 }
