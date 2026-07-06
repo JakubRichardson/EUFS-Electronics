cea2887b8ff9b5121f5c05d71f1dcf24bd6c1fee
Jakub Richardson <jakubr060104@gmail.com>
2026-07-06 11:09:45 +0100

    Update VCU-IO for use with the VCU

--- VCU-IO/VCU-IO.ino
+++ VCU-IO/VCU-IO.ino
@@ -1,46 +1,57 @@
 #include <Arduino.h>
 #include <FlexCAN_T4.h>
-#include <Fsm.h>
 
-// Print Debug
-const bool LOG_STATE = true;
+// Wheelspeed struct
+struct WheelState {
+  volatile unsigned long lastEdgeUs = 0;
+  volatile unsigned long lastIntervalUs = 0;
+  volatile unsigned long lastValidPulseUs = 0;
+  volatile unsigned int rpm_x10 = 0;
+};
 
-// CAN Bus
-FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
-const int BAUD_RATE = 500000;
+// Inverter/RES messages
+unsigned long lastRequestTime = 0;
+const unsigned long REQUEST_CYCLE_TIME = 100;
 
-// APPS Timeout Timer
-const uint32_t APPS_ID = 0x41;    // CAN ID to monitor APPS-Status
-const uint32_t BRAKES_ID = 0x44;    // CAN ID to monitor APPS-Status
-const unsigned int BRAKE_THRESHOLD = 3; // bar
-bool brakesEngagedFlag = false;
-const unsigned long TIMEOUT_MS = 200; // APPS Monitor - Timeout
-bool appsGood = false;                // APPS flag
-unsigned long lastAppsMessageTime = 0;    // Timeout timer
-
-// CAN Messages
+// Inverter Messages
 const uint32_t Inv_rcv = 0x181;
 const uint32_t Inv_snd = 0x201;
-const uint32_t VCU_State = 0x3EC;
+const int RUN_ID = 0xE8;
+const int READ_ID = 0x3D;
+const int INV_ENBL_ID = 0x51;
+bool invReceivingRun = false;
+bool invReceivingEnbl = false;
+
+// CANopen RES receiver (DE7.4.8): Node-ID fixed 0x11, 500 kbit/s
+const int RES_NODE_ID = 0x11;
+const int CAN_NMT_BOOTUP = 0x700 + RES_NODE_ID;   // 0x711, boot-up (data 0x00)
+const int CAN_NMT_CMD = 0x000;                    // NMT master -> slaves
+const int CAN_TPDO1_STATUS = 0x180 + RES_NODE_ID; // 0x191, PDOs 2000-2007 @ 30 ms
+bool resReceiving = false;
+
+// CAN
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
+const int CAN_BAUDRATE = 500000;
+
+// CAN IDs (application)
 const int VCUIO2VCU_Buttons = 0x3EB; // 1003
 
 // Pins
 const int CAN_BUS_STBY = 6;
-int ANG_INP[2] = {14, 15};                         // Analog Inputs
-int DIG_INP[8] = {11, 12, 21, 20, 19, 18, 17, 16}; // 24/5V Digital Inputs
-int DIG_OUT_5V[8] = {0, 1, 2, 3, 4, 5, 7, 8};      // 5V Digital Outputs
-int DIG_OUT_24V[2] = {9, 10};                      // 24V Digital Outputs
+// Pin arrays (kept as requested)
+int ANG_INP[2] = {14, 15};
+int DIG_INP[8] = {11, 12, 21, 20, 19, 18, 17, 16};
+int DIG_OUT_5V[8] = {0, 1, 2, 3, 4, 5, 7, 8};
+int DIG_OUT_24V[2] = {9, 10};
 
-// Input signals:
+// Assign signals using array indices
 const int TS_ACTIVATION_SIDE = DIG_INP[0];
 const int TS_ACTIVATION_DASH = DIG_INP[1];
+const int TS_ACTIVATION_OUT = DIG_OUT_5V[0];
 const int R2D_BUTTON = DIG_INP[2];
 const int ASMS_SWITCH = DIG_INP[3];
-
 const int WS_RR_PIN = DIG_INP[4];
 const int WS_RL_PIN = DIG_INP[5];
-
-const int TS_ACTIVATION_OUT = DIG_OUT_5V[0];
 
 // Button debounce (active LOW; same timing as R2D)
 int buttonState;
@@ -56,6 +67,7 @@ int sideLastButtonState = LOW;
 unsigned long sideLastDebounceTime = 0;
 
 const unsigned long DEBOUNCE_DELAY = 20;
+const unsigned long R2D_HOLD_MS = 400;
 
 // =====================
 // STATE VARIABLES
@@ -64,7 +76,7 @@ const unsigned long DEBOUNCE_DELAY = 20;
 bool r2dPressed = false;
 bool r2dHeld = false;
 bool dashPressed = false;
-bool dashHeld = false; // TODO: Check needed
+bool dashHeld = false;
 bool sidePressed = false;
 bool sideHeld = false;
 bool asmsState = false;
@@ -72,81 +84,50 @@ bool tsActivation = false;
 unsigned long r2dHoldUntilMs = 0;
 unsigned long dashHoldUntilMs = 0;
 unsigned long sideHoldUntilMs = 0;
-
-// RES receiver: last time we saw TPDO1 (0x191) or we acted on boot / sent NMT (for first-frame timeout)
-unsigned long lastResActivityMs = 0;
-
-// RES Messages
-const int RES_PDO = 0x191;
-bool resReceiving = false;
-const unsigned long R2D_HOLD_MS = 400;
-
-// Inverter RFE Message
-bool RFE = false;
-const int RUN_ID = 0xE8; // TODO
-const int READ_ID = 0x3D;
-const int GO_drive_enable = 0xE3;
-const int RDY = 0xE2;
-
-// Inverter Enable Message
-unsigned long lastEnableSendTime = 0;        // the last time enable was sent to the inverter
-const unsigned long ENABLE_CYCLE_TIME = 100; // time (ms) between sending enable messages
-const int INV_ENBL_ID = 0x51;                // TODO
-const unsigned int INV_ENBL_MSG = 64;
-const unsigned int INV_DISBL_MSG = 4;
-bool invDriveEnabled = false;
-
-unsigned long lastRequestTime = 0;
-const unsigned long REQUEST_CYCLE_TIME = 100;
-bool invReceivingRun = false;
-bool invReceivingEnbl = false;
-
-int lastTxUs = 0;
 unsigned long lastButtonsCanTxMs = 0;
-int lastButtonsCanData = 0;
-bool hasLastButtonsCanData = false;
-bool lastR2dHeldDebug = false;
-bool lastTsActivationDebug = false;
-bool lastAsmsStateDebug = false;
-unsigned long lastWheelSerialMs = 0;
 
-// State definitions implemented as enum: B000 = OFF, B001 = ACTIVE, B010 = MANUAL, B111 = CORSA
-typedef enum : uint8_t { 
-  TS_OFF = 1,
-  TS_ACTIVE = 2,
-  MANUAL = 3,
-  CORSA = 7
-} VCU_State_Enum;
+// =====================
+// FUNCTIONS
+// =====================
 
-VCU_State_Enum CURRENT_STATE = TS_OFF;      // Current state
+void setupInputs(int *pins, int size) {
+  for (int i = 0; i < size; i++) {
+    pinMode(pins[i], INPUT);
+  }
+}
 
-// Events
-#define TS_ACTIVATION_EVENT 0
-#define TS_DEACTIVATION_EVENT 1
-#define R2D_BUTTON_EVENT 2
+void setupOutputs(int *pins, int size) {
+  for (int i = 0; i < size; i++) {
+    pinMode(pins[i], OUTPUT);
+    digitalWrite(pins[i], LOW);
+  }
+}
 
-// FSM States
-State stateTractiveSystemOff(&onEnterTractiveSystemOff, &duringTractiveSystemOff, NULL);
-State stateTractiveSystemActive(&onEnterTractiveSystemActive, &duringTractiveSystemActive, NULL);
-State stateManualDriving(&onEnterManualDriving, &duringManualDriving, NULL);
-Fsm fsm(&stateTractiveSystemOff);
+// =====================
+// CAN Receive
+// =====================
 
-// Current State Heartbeat
-unsigned long lastHeartBeatTime = 0;
-const int heartRate = 10; // ms
-void heartBeat() {
-  if (millis() - lastHeartBeatTime < heartRate) return;
-  lastHeartBeatTime = millis();
-  CAN_message_t msg;
-  msg.id = VCU_State;
-  msg.buf[0] = CURRENT_STATE;
-  msg.buf[1] = 0;
-  msg.len = 2;
-  Can0.write(msg);
+void handleReceive(const CAN_message_t &msg) {
+  Serial.println("Received");
+  if (msg.id == Inv_rcv) {
+    if(msg.buf[0] == RUN_ID){
+      if (!invReceivingRun) invReceivingRun = true;
+    } else if (msg.buf[0] == INV_ENBL_ID){
+      if (!invReceivingEnbl) invReceivingEnbl = true;
+    }
+  }
+}
+
+void handleRES(const CAN_message_t &msg) {
+  if (msg.id == CAN_TPDO1_STATUS) {
+    if (!resReceiving) {
+      resReceiving = true;
+    }
+  }
 }
 
 void inverterRequest(int request) {
-  // Request RUN flag
+  // Request RUN flag and KERN mode
   CAN_message_t msg;
   msg.id = Inv_snd;
   msg.buf[0] = READ_ID;
@@ -156,186 +137,24 @@ void inverterRequest(int request) {
   Can0.write(msg);
 }
 
+// =====================
+// RES Startup
+// =====================
+
+// NMT: start node 0x11 (operational). CAN-ID 0x000, data 0x01 0x11
 void resRequest() {
   // Request RES mode
   CAN_message_t msg;
-  msg.id = 0x000;
+  msg.id = CAN_NMT_CMD;
   msg.buf[0] = 0x01;
-  msg.buf[1] = 0x11;
+  msg.buf[1] = RES_NODE_ID;
   msg.len = 2;
   Can0.write(msg);
 }
 
-void checkFlag() {
-  if (millis() - lastAppsMessageTime < TIMEOUT_MS) return;
-  appsGood = false;
-  if (LOG_STATE) {
-    Serial.println("APPS Timeout");
-  }
-}
-
-void appsCallback(const CAN_message_t &msg) {
-  if (msg.id == APPS_ID) {
-    lastAppsMessageTime = millis();
-    appsGood = !(msg.buf[1] & 0x04);
-  }
-}
-
-void brakesCallback(const CAN_message_t &msg) {
-  if (msg.id == BRAKES_ID) {
-    unsigned int frontBrakePressure = ((msg.buf[1] << 8) | msg.buf[0]);
-    brakesEngagedFlag = (frontBrakePressure >= BRAKE_THRESHOLD);
-  }
-}
-
-void handleReceive(const CAN_message_t &msg) {
-  if (msg.id == Inv_rcv) {
-    if(msg.buf[0] == RUN_ID){
-      if (!invReceivingRun) {
-        invReceivingRun = true;
-      }
-      RFE = msg.buf[1] & 0x01;
-    }
-    else if (msg.buf[0] == INV_ENBL_ID){
-      if (!invReceivingEnbl) {
-        invReceivingEnbl = true;
-      }
-      if(msg.buf[1] == INV_ENBL_MSG) {
-        invDriveEnabled = true;
-      } else {
-        invDriveEnabled = false;
-      }
-    }
-  }
-}
-
-/**
- * @brief Send inverter enable CAN message
- *
- * @param bool enable - send enable/disable
- */
-void enableInverter(bool enable) {
-  if ((invDriveEnabled != enable) && ((millis() - lastEnableSendTime) > ENABLE_CYCLE_TIME)) {
-    lastEnableSendTime = millis();
-    CAN_message_t msg;
-    msg.id = Inv_snd;
-    msg.buf[0] = INV_ENBL_ID;
-    if (enable) {
-      msg.buf[1] = INV_ENBL_MSG;
-    } else {
-      msg.buf[1] = INV_DISBL_MSG;
-    }
-    msg.len = 3;
-    Can0.write(msg);
-  }
-}
-
-/**
- * @brief Reads button to check if pressed
- *
- * @return bool - button pressed
- */
-bool buttonPressed() {
-  bool pressed = false;
-  // Handle R2D Button
-  int buttonReading = digitalRead(R2D_BUTTON);
-  if (buttonReading != lastButtonState) {
-    lastDebounceTime = millis(); // Reset debounce timer
-  }
-  if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
-    if (buttonReading != buttonState) {
-      buttonState = buttonReading;
-      if (buttonState == LOW){ // Button Released
-        pressed = true;
-      }
-    }
-  }
-  lastButtonState = buttonReading;
-  return pressed;
-}
-
-void onEnterTractiveSystemOff() {
-  if (LOG_STATE) {
-    Serial.println("TS-Off");
-  }
-  CURRENT_STATE = TS_OFF;
-}
-
-void duringTractiveSystemOff() {
-  // 1. Read RFE. If RFE goes high go to TS-Active
-  if (RFE) {
-    fsm.trigger(TS_ACTIVATION_EVENT);
-  }
-  // 2. Send Inverter Disable
-  enableInverter(false);
-}
-
-void onEnterTractiveSystemActive() {
-  if (LOG_STATE) {
-    Serial.println("TS Active");
-  }
-  CURRENT_STATE = TS_ACTIVE;
-}
-
-unsigned long lastDebug = false;
-void duringTractiveSystemActive() {
-  // 1. Read RFE. If goes low go to TS-Off
-  if (!RFE) {
-    fsm.trigger(TS_DEACTIVATION_EVENT);
-  }
-  // 2. Read button. If pressed go to Manual Driving
-  if (r2dHeld && brakesEngagedFlag) {
-    fsm.trigger(R2D_BUTTON_EVENT);
-  }
-  // 3. Send Inverter Disable
-  enableInverter(false);
-}
-
-void onEnterManualDriving() {
-  if (LOG_STATE) {
-    Serial.println("Manual Driving");
-  }
-  CURRENT_STATE = MANUAL;
-  lastAppsMessageTime = millis();
-  enableInverter(true);
-  delay(1);
-  enableInverter(false);
-  delay(1);
-}
-
-void duringManualDriving() {
-  // 1. Read RFE. If goes low go to TS-Off
-  if (!RFE) {
-    Serial.print("RFE: ");
-    Serial.println(RFE);
-    // Don't need to disable Inverter as RFE low.
-    // Will need to go back through TS-active state again to enable again
-    fsm.trigger(TS_DEACTIVATION_EVENT);
-  }
-  // 2. Read button. If pressed go to TS-Active
-  if (r2dPressed) {
-    Serial.print("Button: ");
-    Serial.println(r2dPressed);
-    fsm.trigger(R2D_BUTTON_EVENT);
-  }
-  // 3. Check APPS flags. If implausible or not receiving -> deactivate drive
-  checkFlag(); // Check APPS flag
-  if (!appsGood) {
-    Serial.print("appsGood: ");
-    Serial.println(appsGood);
-    fsm.trigger(R2D_BUTTON_EVENT);
-  }
-  // 4. Send Inverter Enable
-  enableInverter(true);
-}
-
-void sendButtonsCANIfNeeded() {
-  if (millis() - lastButtonsCanTxMs > 100) {
-    uint8_t data = buildButtonsCanData();
-    sendButtonsCAN(data);
-    lastButtonsCanTxMs = millis();
-  }
-}
+// =====================
+// Buttons
+// =====================
 
 // Returns debounced active-low level. If edgeToLowOut != nullptr, sets *edgeToLowOut
 // true for one call when the stable state transitions to LOW (R2D pulse semantics).
@@ -391,7 +210,9 @@ int buildButtonsCanData() {
   return data;
 }
 
-void sendButtonsCAN(uint8_t data) {
+void sendButtonsCAN() {
+  uint8_t data = buildButtonsCanData();
+
   CAN_message_t msg;
   msg.id = VCUIO2VCU_Buttons;
   msg.len = 1;
@@ -399,73 +220,110 @@ void sendButtonsCAN(uint8_t data) {
   Can0.write(msg);
 }
 
-void setupFSM() {
-  // State: Tractive System Off
-  fsm.add_transition(&stateTractiveSystemOff, &stateTractiveSystemActive, TS_ACTIVATION_EVENT, NULL);
+// =====================
+// Wheel speeds
+// =====================
 
-  // State: Tractive System Active
-  fsm.add_transition(&stateTractiveSystemActive, &stateManualDriving, R2D_BUTTON_EVENT, NULL);
-  fsm.add_transition(&stateTractiveSystemActive, &stateTractiveSystemOff, TS_DEACTIVATION_EVENT, NULL);
+const int PULSES_PER_REV = 12;
+const int MAX_WHEEL_RPM = 2500;
+const int STOP_TIMEOUT_US = 200000;
+const unsigned long MIN_PULSE_SPACING_US =
+  60000000UL / (PULSES_PER_REV * MAX_WHEEL_RPM);
 
-  // State: Manual Driving
-  fsm.add_transition(&stateManualDriving, &stateTractiveSystemActive, R2D_BUTTON_EVENT, NULL);
-  fsm.add_transition(&stateManualDriving, &stateTractiveSystemOff, TS_DEACTIVATION_EVENT, NULL);
-}
+WheelState rlState;
+WheelState rrState;
 
-void setupInputs(int *pins, int size) {
-  for (int i = 0; i < size; i++) {
-    pinMode(pins[i], INPUT);
+static inline void capturePulse(volatile WheelState &w) {
+  int nowUs = micros();
+  if (w.lastEdgeUs == 0) {
+    w.lastEdgeUs = nowUs;
+    w.lastValidPulseUs = nowUs;
+    return;
   }
+
+  unsigned long dtUs = nowUs - w.lastEdgeUs;
+  if (dtUs < MIN_PULSE_SPACING_US) return;
+  w.lastIntervalUs = dtUs;
+  w.lastEdgeUs = nowUs;
+  w.lastValidPulseUs = nowUs;
 }
 
-void setupOutputs(int *pins, int size) {
-  for (int i = 0; i < size; i++) {
-    pinMode(pins[i], OUTPUT);
-    digitalWrite(pins[i], LOW);
+void rlISR() { capturePulse(rlState); }
+void rrISR() { capturePulse(rrState); }
+
+static inline void updateWheelSpeed(volatile WheelState &w) {
+  unsigned long intervalUs;
+  unsigned long lastValidPulseUs;
+  // Atomic copy of interrupt-updated values
+  noInterrupts();
+  intervalUs = w.lastIntervalUs;
+  lastValidPulseUs = w.lastValidPulseUs;
+  interrupts();
+
+  unsigned long nowUs = micros();
+  unsigned int rpm_x10_calc = 0;
+
+  if (lastValidPulseUs == 0 || (nowUs - lastValidPulseUs) > STOP_TIMEOUT_US) {
+    rpm_x10_calc = 0;
+  } else if (intervalUs != 0) {
+    rpm_x10_calc = 
+      (unsigned int)(600000000UL / 
+      ((unsigned long)PULSES_PER_REV * intervalUs));
+
+    if (rpm_x10_calc > 65535) rpm_x10_calc = 65535;
   }
+
+  // Atomic update
+  noInterrupts();
+  w.rpm_x10 = rpm_x10_calc;
+  interrupts();
 }
+
+// =====================
+// SETUP
+// =====================
 
 void setup() {
-  // Initialise Serial Interface
   Serial.begin(9600);
 
-  // Initilise Pins
-  // setupInputs(ANG_INP, 2); // Analog Inputs
-  setupInputs(DIG_INP, 8);      // Digital Inputs
-  setupOutputs(DIG_OUT_5V, 8);  // 5V Digital Outputs
-  setupOutputs(DIG_OUT_24V, 2); // 24V Digital Outputs
+  // Setup IO using arrays
+  setupInputs(DIG_INP, 8);
+  setupOutputs(DIG_OUT_5V, 8);
+  setupOutputs(DIG_OUT_24V, 2);
 
   // Initialise CAN
   pinMode(CAN_BUS_STBY, OUTPUT);
   digitalWrite(CAN_BUS_STBY, LOW); /* optional tranceiver enable pin */
   Can0.begin();
-  Can0.setBaudRate(BAUD_RATE);
+  Can0.setBaudRate(CAN_BAUDRATE);
   Can0.setMaxMB(16);
   Can0.enableMBInterrupts(); // enables all mailboxes to be interrupt enabled
-  Can0.setMBFilter(MB0, APPS_ID);
-  Can0.onReceive(MB0, appsCallback);
-  Can0.setMBFilter(MB1, Inv_rcv);
-  Can0.onReceive(MB1, handleReceive);
-  Can0.setMBFilter(MB2, BRAKES_ID);
-  Can0.onReceive(MB2, brakesCallback);
+  Can0.setMBFilter(MB0, Inv_rcv);
+  Can0.onReceive(MB0, handleReceive);
+  Can0.setMBFilter(MB1, CAN_TPDO1_STATUS);
+  Can0.onReceive(MB1, handleRES);
   Can0.mailboxStatus();
 
-  // Setup State Machine
-  setupFSM();
-  onEnterTractiveSystemOff();
+  // Setup Wheelspeeds
+  pinMode(WS_RL_PIN, INPUT);
+  pinMode(WS_RR_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(WS_RL_PIN), rlISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(WS_RR_PIN), rrISR, RISING);
 }
 
+// =====================
+// LOOP
+// =====================
+
 void loop() {
-  fsm.run_machine();
   if ((millis() - lastRequestTime) > REQUEST_CYCLE_TIME) {
     lastRequestTime = millis();
     if(!invReceivingRun) inverterRequest(RUN_ID);
     if(!invReceivingEnbl) inverterRequest(INV_ENBL_ID);
-    // if(!resReceiving) resRequest();
+    if(!resReceiving) resRequest();
   }
-  heartBeat(); // Send status heartbeat
 
-  // Read inputs
+  // Read buttons
   r2dPressed = 
     readActiveLowButtonEdge(R2D_BUTTON, buttonState, lastButtonState, lastDebounceTime);
   r2dHeld = updateHeldHighFromEdge(r2dPressed, R2D_HOLD_MS, r2dHoldUntilMs);
@@ -482,7 +340,6 @@ void loop() {
   // =====================
   // TS Activation Logic (mux; pulse one loop per press, same as R2D-style edges)
   // =====================
-
   if (!asmsState) {
     tsActivation = dashHeld;
   } else {
@@ -492,9 +349,15 @@ void loop() {
   // Output
   digitalWrite(TS_ACTIVATION_OUT, tsActivation);
 
+  // Wheelspeed
+  updateWheelSpeed(rlState);
+  updateWheelSpeed(rrState);
+
   // =====================
   // CAN TX
   // =====================
-
-  sendButtonsCANIfNeeded();
+  if (millis() - lastButtonsCanTxMs > 100) {
+    lastButtonsCanTxMs = millis();
+    sendButtonsCAN();
+  }
 }
